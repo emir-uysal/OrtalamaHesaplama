055b660103dc27f25b65594be5f4f9663b9e105a
emir-uysal <emiruysal220519@gmail.com>
2024-08-22 09:33:11 +0300

    first commit

---
+++ lib/widgets/kredi_dropdown_widget.dart
@@ -0,0 +1,39 @@
+import 'package:dinamik_ortalama_hesapla/constants/app_constants.dart';
+import 'package:dinamik_ortalama_hesapla/helper/data_helper.dart';
+import 'package:flutter/material.dart';
+
+class KrediDropdownWidget extends StatefulWidget {
+  final Function onKrediSecildi;
+  const KrediDropdownWidget({required this.onKrediSecildi, Key? key}) : super(key: key);
+
+  @override
+  State<KrediDropdownWidget> createState() => _KrediDropdownWidgetState();
+}
+
+class _KrediDropdownWidgetState extends State<KrediDropdownWidget> {
+  double secilenKrediDegeri = 1;
+  @override
+  Widget build(BuildContext context) {
+    return  Container(
+      alignment: Alignment.center,
+      padding: Sabitler.dropDownPadding,
+      decoration: BoxDecoration(
+        color: Sabitler.anaRenk.shade100.withOpacity(0.3),
+        borderRadius: Sabitler.borderRadius
+      ),
+      child: DropdownButton<double>(
+        value: secilenKrediDegeri,
+        elevation: 16,
+        iconEnabledColor: Sabitler.anaRenk.shade200,
+        onChanged: (deger){
+          setState(() {
+            secilenKrediDegeri = deger!;
+            widget.onKrediSecildi(deger);
+          });
+        },
+        underline: Container(),
+        items: DataHelper.tumDerslerinKredileri(), 
+        ),
+    );
+  }
+}
